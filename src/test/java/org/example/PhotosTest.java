d34491523c847f2de3e53aa17a0630f4f5e9b2c0
kuxov <grigoriy@kuksov.com>
2022-11-18 02:12:21 +0300

    первый тест

--- src/test/java/org/example/PhotosTest.java
+++ src/test/java/org/example/PhotosTest.java
@@ -4,19 +4,25 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class LoginTest {
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
+
+public class PhotosTest {
     public static LoginPage loginPage;
     public static ProfilePage profilePage;
+    public static PhotosPage photosPage;
     public static WebDriver driver;
 
     @BeforeEach
-   public void setup()  {
+    public void setup()  {
         System.setProperty("webdriver.chrome.driver", ConfProperties.getProperty("chromedriver"));
         driver = new ChromeDriver();
         loginPage = new LoginPage(driver);
         profilePage = new ProfilePage(driver);
+        photosPage = new PhotosPage(driver);
         driver.manage().window().maximize();
         driver.get(ConfProperties.getProperty("loginpage")); }
     @AfterEach
@@ -25,18 +31,17 @@ public class LoginTest {
     }
 
     @Test
-    public void invalidPassTest() {
-        loginPage.inputLogin(ConfProperties.getProperty("login"));
-        loginPage.inputPasswd("123456");
-        loginPage.clickLoginBtn();
-        assertEquals("Неправильно указан логин и/или пароль", loginPage.getErrMsg());
-        }
-    @Test
-    public void loginTest() {
+    public void createAlbumTest() {
         loginPage.inputLogin(ConfProperties.getProperty("login"));
         loginPage.inputPasswd(ConfProperties.getProperty("password"));
         loginPage.clickLoginBtn();
-        String user = profilePage.getUserName();
-        assertEquals("technoPol20 technoPol20", user);
-        }
+
+        profilePage.toPhotosPage();
+
+        photosPage.startCreatingAlbum();
+        photosPage.inputAlbumTitle("test");
+        photosPage.createAlbum();
+
+        assumeTrue(photosPage.albumIsEmpty());
+    }
 }
--- src/test/java/org/example/ProfilePage.java
+++ src/test/java/org/example/ProfilePage.java
@@ -3,15 +3,28 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+import static org.example.PhotosTest.driver;
 
 public class ProfilePage {
     public WebDriver driver;
     public ProfilePage(WebDriver driver) {
         PageFactory.initElements(driver, this);
         this.driver = driver; }
+
+    @FindBy(xpath = "//*[contains(text(), 'Фото')]")
+    private WebElement goToPhotos;
     @FindBy(xpath = "//*[contains(@class, 'tico ellip')]")
     private WebElement userMenu;
     public String getUserName() {
         String userName = userMenu.getText();
         return userName; }
+
+    public void toPhotosPage() {
+         new WebDriverWait(driver, Duration.ofSeconds(10)).until(ExpectedConditions.elementToBeClickable(goToPhotos));
+        goToPhotos.click(); }
 }
