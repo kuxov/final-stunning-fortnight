0db7a141c9a2fd435e472ab0f3917bd315836ea9
kuxov <grigoriy@kuksov.com>
2022-11-18 05:08:02 +0300

    а почему эти файлы не отправились...

---
+++ src/test/java/org/example/Pages/PhotosPage.java
@@ -0,0 +1,68 @@
+package org.example.Pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+import static org.example.Tests.PhotosTest.driver;
+
+public class PhotosPage {
+    @FindBy(xpath = "//*[contains(@class, 'button-pro __sec __small')]")
+    public WebElement createPhotoAlbum;
+
+    @FindBy(xpath = "//textarea")
+    public WebElement textFieldEditor;
+
+    @FindBy(xpath = "//*[@id='hook_FormButton_button_album_create']")
+    public WebElement confirm;
+
+    @FindBy(xpath = "//*[contains(@id, 'field_query')]")
+    public WebElement searchField;
+    public PhotosPage(WebDriver driver) {
+        PageFactory.initElements(driver, this);
+    }
+
+    public void inputSearchTitle(String name)
+    {
+        new WebDriverWait(driver, Duration.ofSeconds(3)).until(ExpectedConditions.visibilityOf(searchField));
+        searchField.sendKeys(name);
+    }
+
+    public boolean albumFound()
+    {
+        WebElement element = new WebDriverWait(driver, Duration.ofSeconds(3))
+                .until(driver -> driver.findElement(By.xpath("//*[contains(text(), 'test2')]")));
+        return element != null;
+    }
+    public boolean albumIsEmpty()
+    {
+        WebElement element = new WebDriverWait(driver, Duration.ofSeconds(3))
+                .until(driver -> driver.findElement(By.xpath("//*[contains(text(), 'Альбом пуст')]")));
+        return element != null;
+    }
+
+    public PhotosPage startCreatingAlbum()
+    {
+        new WebDriverWait(driver, Duration.ofSeconds(3)).until(ExpectedConditions.elementToBeClickable(createPhotoAlbum));
+        createPhotoAlbum.click();
+        return this;
+    }
+
+    public PhotosPage inputAlbumTitle(String text)
+    {
+        new WebDriverWait(driver, Duration.ofSeconds(3)).until(ExpectedConditions.visibilityOf(textFieldEditor));
+        textFieldEditor.sendKeys(text);
+        return this;
+    }
+
+    public void createAlbum()
+    {
+        confirm.click();
+    }
+}
